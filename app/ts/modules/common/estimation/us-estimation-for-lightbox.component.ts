95ac7ab67dedcd3bf87e7e83fd47898197ec2a8f
Jesús Espino <jespinog@gmail.com>
2017-08-09 10:07:44 +0200

    Adding accesor to us-estimation-for-lightbox

--- app/ts/modules/common/estimation/us-estimation-for-lightbox.component.ts
+++ app/ts/modules/common/estimation/us-estimation-for-lightbox.component.ts
@@ -1,21 +1,52 @@
-import {Component, Input, OnChanges} from "@angular/core";
+import {Component, Input, OnChanges, forwardRef} from "@angular/core";
+import {ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
 import * as Immutable from "immutable";
 
 @Component({
     selector: "tg-us-estimation-for-lightbox",
     template: require("./us-estimation-for-lightbox.pug")(),
+    providers: [
+      {
+        provide: NG_VALUE_ACCESSOR,
+        useExisting: forwardRef(() => UsEstimationForLightbox),
+        multi: true
+      }
+    ]
 })
-export class UsEstimationForLightbox implements OnChanges {
+export class UsEstimationForLightbox implements ControlValueAccessor, OnChanges {
     @Input() roles;
     @Input() points;
     private roleOpen: number = null;
     computableRoles: Immutable.List<any> = Immutable.List();
+    _value: any;
+    onChange: any;
 
     ngOnChanges() {
         if (this.roles) {
             this.computableRoles = this.roles.filter((role) => role.get("computable"));
         }
     }
+    writeValue(value: any) {
+        this.value = value;
+    }
+
+    get value() {
+        return this._value;
+    }
+
+    set value(val) {
+        this._value = val;
+        if (this.onChange) {
+            this.onChange(val);
+        }
+    }
+
+    registerOnChange(fn: any) {
+        this.onChange = fn;
+    }
+
+    registerOnTouched() {}
+
 
     openRole(roleId) {
         this.roleOpen = roleId;
